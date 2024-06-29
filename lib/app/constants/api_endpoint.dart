c2b41af6aac48b173f4c50c586f6a0828b789bbe
Ashish Amgain <amgainashish@gmail.com>
2024-06-29 20:40:50 +0545

    add

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -4,11 +4,13 @@ class ApiEndpoints {
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
   static const String baseUrl = "http://10.0.2.2:5000/api/";
+  static const int roomlimit = 5;
   // static const String baseUrl = "http://192.168.1.254:5000/api/";
 
   // ====================== Auth Routes ======================
   static const String login = "user/login";
   static const String register = "user/register";
+  static const String getRooms = "room/";
   // static const String getAllStudent = "auth/getAllStudents";
   // static const String getStudentsByBatch = "auth/getStudentsByBatch/";
   // static const String getStudentsByCourse = "auth/getStudentsByCourse/";
