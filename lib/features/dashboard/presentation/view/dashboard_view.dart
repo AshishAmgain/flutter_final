dbad61aaef222f22fd6e283adf72c330069edeed
Ashish Amgain <amgainashish@gmail.com>
2024-07-04 12:56:17 +0545

    changind dashboard view

--- lib/features/dashboard/presentation/view/dashboard_view.dart
+++ lib/features/dashboard/presentation/view/dashboard_view.dart
@@ -1,167 +1,59 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// class DashboardView extends ConsumerStatefulWidget {
-//   const DashboardView({Key? key}) : super(key: key);
-
-//   @override
-//   ConsumerState<DashboardView> createState() => _DashboardViewState();
-// }
-
-// class _DashboardViewState extends ConsumerState<DashboardView> {
-//   int _selectedIndex = 0;
-
-//   static const List<Widget> _widgetOptions = <Widget>[
-//     Text('Home Page',
-//         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-//     Text('Rooms Page',
-//         style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
-//     Text('Settings Page',
-//         style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-//   ];
-
-//   void _onItemTapped(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Dashboard'),
-//         centerTitle: true,
-//       ),
-//       body: Center(
-//         child: _widgetOptions.elementAt(_selectedIndex),
-//       ),
-//       bottomNavigationBar: BottomNavigationBar(
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.king_bed),
-//             label: 'Rooms',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.settings),
-//             label: 'Settings',
-//           ),
-//         ],
-//         currentIndex: _selectedIndex,
-//         selectedItemColor: Colors.amber[800],
-//         onTap: _onItemTapped,
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hotel_book/features/dashboard/presentation/viewmodel/room_view_model.dart'; // Adjust based on your provider setup
+import 'package:hotel_book/features/dashboard/presentation/view/screen/room_screen.dart';
 
-class RoomView extends ConsumerStatefulWidget {
-  const RoomView({Key? key}) : super(key: key);
+class DashboardView extends ConsumerStatefulWidget {
+  const DashboardView({Key? key}) : super(key: key);
 
   @override
-  _RoomViewState createState() => _RoomViewState();
+  ConsumerState<DashboardView> createState() => _DashboardViewState();
 }
 
-class _RoomViewState extends ConsumerState<RoomView> {
-  final ScrollController _scrollController = ScrollController();
+class _DashboardViewState extends ConsumerState<DashboardView> {
+  int _selectedIndex = 0;
 
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    RoomView(),
+    Text('Settings Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(roomsViewModelProvider);
-
-    return NotificationListener(
-      onNotification: (notification) {
-        if (notification is ScrollEndNotification) {
-          if (_scrollController.position.extentAfter == 0) {
-            ref.read(roomsViewModelProvider.notifier).getRooms();
-          }
-        }
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Rooms'),
-          actions: [
-            TextButton.icon(
-              onPressed: () {
-                ref.read(roomsViewModelProvider.notifier).resetState();
-              },
-              icon: const Icon(Icons.refresh),
-              label: const Text('Refresh'),
-            ),
-          ],
-        ),
-        body: RefreshIndicator(
-          color: Colors.green,
-          backgroundColor: Colors.amberAccent,
-          onRefresh: () async {
-            ref.read(roomsViewModelProvider.notifier).resetState();
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView.separated(
-                    separatorBuilder: (context, index) => const Divider(),
-                    controller: _scrollController,
-                    itemCount: state.rooms.length,
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      final room = state.rooms[index];
-                      return ListTile(
-                        title: Text(room.name),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(room.description),
-                            SizedBox(height: 8),
-                            // Example to show images, adjust as per your Room model
-                            SizedBox(
-                              height: 100,
-                              child: ListView.builder(
-                                scrollDirection: Axis.horizontal,
-                                itemCount: room.images.length,
-                                itemBuilder: (context, imageIndex) {
-                                  final imageUrl = room.images[imageIndex];
-                                  return Padding(
-                                    padding: const EdgeInsets.only(right: 8.0),
-                                    child: Image.network(
-                                      imageUrl,
-                                      width: 100,
-                                      height: 100,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                if (state.isLoading)
-                  const CircularProgressIndicator(color: Colors.red),
-              ],
-            ),
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Dashboard'),
+      //   centerTitle: true,
+      // ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.king_bed),
+            label: 'Hotels',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
