e210a49e81bfa3447eb4248f62af50802b1fd907
Ashish Amgain <amgainashish@gmail.com>
2024-07-12 10:15:33 +0545

    adding test

--- test/unit_test/auth_test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:hotel_book/features/auth/presentation/viewmodel/auth_view_model.dart';
-
-@GenerateMocks([AuthViewModel])
-void main() {}
--- test/unit_test/auth_usecase_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:hotel_book/features/auth/domain/use_case/auth_use_case.dart';
-import 'package:mockito/annotations.dart';
-
-// Add this line to generate the mocks
-@GenerateMocks([AuthUseCase])
-void main() {}
